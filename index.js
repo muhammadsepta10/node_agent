7757416d814bc3c215d0018f09ef97ef0fc001d2
root <root@localhost.localdomain>
2021-08-02 22:22:19 +0700

    change cron interval & add fixed decimal

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const axios = require("axios")
 const cron = require("node-cron")
 require("dotenv").config()
 
-cron.schedule("* * * * * *",async()=>{
+cron.schedule("* */1 * * * *",async()=>{
   try {
     const getCpu = await cpu()
     const ram = getMemory()
-    const ram_usage = (+ram.used)/1024/1024
-    const ram_free = (+ram.free)/1024/1024
-    const ram_total = (+ram.total)/1024/1024
+    const ram_usage = ((+ram.used)/1024/1024).toFixed(2)
+    const ram_free = ((+ram.free)/1024/1024).toFixed(2)
+    const ram_total = ((+ram.total)/1024/1024).toFixed(2)
     const hdd = getDisk().filter(val=>{
       return val.mounted=="/"
     })[0]
-    const hdd_total = (+hdd?.kblocks||0)/1024/1024
-    const hdd_usage = (+hdd?.used||0)/1024/1024
-    const hdd_free = (+hdd?.available||0)/1024/1024
-    const cpu_total = getCpu.total
-    const cpu_usage = getCpu.usage
-    const cpu_free = getCpu.free
+    const hdd_total = ((+hdd?.kblocks||0)/1024/1024).toFixed(2)
+    const hdd_usage = ((+hdd?.used||0)/1024/1024).toFixed(2)
+    const hdd_free = ((+hdd?.available||0)/1024/1024).toFixed(2)
+    const cpu_total = getCpu.total.toFixed(2)
+    const cpu_usage = getCpu.usage.toFixed(2)
+    const cpu_free = getCpu.free.toFixed(2)
     const data = {
       "ip": getIp(),
       ram_usage,
